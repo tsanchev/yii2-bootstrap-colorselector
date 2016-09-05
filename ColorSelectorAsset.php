1763290b31d511034fb40dbfe26a1b896ff0e978
tsanchev <tsanchev@gmail.com>
2016-09-05 21:29:30 +0300

    Add files via upload

---
+++ ColorSelectorAsset.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * Yii 2 asset for bower LightBox2
+ *
+ * @link      https://github.com/tsanchev/yii2-lightbox2
+ * @package   yii2-asset-lightbox2
+ * @license   BSD-3-Clause
+ */
+
+namespace tsanchev\bootstrap-colorselector;
+
+class ColorSelectorAsset extends \yii\web\AssetBundle
+{
+    /**
+     * {@inheritdoc}
+     */
+    public $sourcePath = '@bower/bootstrap-colorselector/lib/bootstrap-colorselector-0.2.0/';
+
+    /**
+     * {@inheritdoc}
+     */
+    public $js = [
+        'js/bootstrap-colorselector.js',
+    ];
+
+    /**
+     * {@inheritdoc}
+     */
+    public $css = [
+        'css/bootstrap-colorselector.css',
+    ];
+}
